d6fe49ee25728683b22ab16b4c74f4b93567618e
Gopala Vadrevu <gopalak.vadrevu@gmail.com>
2026-07-21 22:52:28 +0530

    Added Sluggify function

--- tools_test.go
+++ tools_test.go
@@ -180,3 +180,30 @@ func TestTools_CreateDirIfnotExists(t *testing.T) {
 	}
 	os.Remove(dirName)
 }
+
+var slugTests = []struct {
+	name          string
+	s             string
+	expected      string
+	errorExpected bool
+}{
+	{name: "valid string", s: "Now is the time", expected: "now-is-the-time", errorExpected: false},
+	{name: "Invalid string", s: "Now is the time", expected: "now-is.the.time", errorExpected: true},
+	{name: "Invalid sluggify", s: "Now is the time", expected: "now-is.the.time", errorExpected: true},
+	{name: "empty sluggify", s: "", expected: "", errorExpected: true},
+}
+
+func TestTools_Slugify(t *testing.T) {
+	var testTool Tools
+
+	for _, e := range slugTests {
+		slug, err := testTool.Slugify(e.s)
+		if err != nil && !e.errorExpected {
+			t.Errorf("%s: error received when none expected: %s", e.name, err.Error())
+		}
+
+		if !e.errorExpected && slug != e.expected {
+			t.Errorf("%s : wrong slug returned; expected %s but got %s", e.name, e.expected, slug)
+		}
+	}
+}
